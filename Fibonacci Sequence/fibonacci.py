92b40746b89856bb1ccd7b14f08f6b04633136d7
Valentin Bryukhanov <bvv.mag@gmail.com>
2015-01-07 20:23:06 +0300

    add couple asserts for Fibonacci

--- Fibonacci Sequence/fibonacci.py
+++ Fibonacci Sequence/fibonacci.py
@@ -4,7 +4,7 @@ Fibonacci Sequence
 """
 
 
-def fib_generator(max_n: int):
+def fib_generator(max_n: int=10):
     """
     Generate Fibonacci numbers lesser than max_n
     :param max_n: Top edge of numbers
@@ -18,5 +18,11 @@ def fib_generator(max_n: int):
 
 if __name__ == "__main__":
     import types
+
     assert isinstance(fib_generator(1), types.GeneratorType)
     assert list(fib_generator(10)) == [1, 1, 2, 3, 5, 8]
+    assert list(fib_generator()) == [1, 1, 2, 3, 5, 8]
+    assert list(fib_generator(1)) == []
+    assert list(fib_generator(0)) == []
+    assert list(fib_generator(2)) == [1, 1]
+    assert next(fib_generator()) == 1
